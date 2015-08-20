bdaa0e1e00a0e88674641d7324e8422a81678ea6
gougoumemeda <a516550257@qq.com>
2015-08-21 02:11:50 +0800

    [compare] submit button

--- compare/compareJS.js
+++ compare/compareJS.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 	$(".compare").delegate(".addButton", "click", addRow)
+	$(".compareButton").click(beginCompare)
 })
 
 var addRow = function(){
-	var newRow = "<form class=\"am-form am-form-horizontal am-g inputRow\">" + $(this).parent().parent().html() + "</form>"
-	$(".compare").append(newRow)
+	var newRow = "<form class=\"am-form am-form-horizontal am-g inputRow\" id = \"\">" + $(this).parent().parent().html() + "</form>"
+	$(".row").append(newRow)
 	$(this).css("display", "none")
+}
+
+var beginCompare = function(){
+	$(".inputRow").each(function(){
+		if ($(this).attr("id") == ""){
+			$(this).attr("id", "compared")
+			$(this).after("<p>Optimal ID:</p>")
+		}
+	})
 }
