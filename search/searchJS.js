cf716ad15c77ec2ca9e8dd212ec8b6bfca9110d3
gougoumemeda <a516550257@qq.com>
2015-09-05 16:06:20 +0800

    [search] key down event

--- search/searchJS.js
+++ search/searchJS.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	$(".keyWord").keydown(function(e){
+		if (e.keyCode == 13){
+			search()
+		}
+	})
 	$(".searchButton .button").click(function(){
 		$(this).siblings().removeClass("buttonClick")
 		$(this).addClass("buttonClick")
@@ -12,9 +17,9 @@ $(document).ready(function(){
 var search = function(){
 	var key = $(".keyWord").val()
 	var type = $(".buttonClick").text()
-	if (type == "Basic"){
+	if (type == "Part"){
 		type = "brick"
-	} else if (type == "Composite"){
+	} else if (type == "Device"){
 		type = "devic"
 	}
 	$.post("search.php", {
